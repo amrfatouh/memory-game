1c27adb172354487b6ce4d64cc200e3024c3425f
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-09-03 09:26:26 +0200

    setting dimensions

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-//filling cards with data-imgid
-document.querySelectorAll('.card').forEach(card => {
-    let imgId = card.querySelector('img').src.split('/')[4];
-    card.setAttribute('data-imgid', imgId);
-})
-
-document.querySelector('.overlay button').onclick = () => {
-    document.querySelector('.info .name span').textContent = prompt('Enter your name: ');
-    document.querySelector('.overlay').remove();
-}
+//starting thr game
+// document.querySelector('.overlay button').onclick = () => {
+//     document.querySelector('.info .name span').textContent = prompt('Enter your name: ');
+//     document.querySelector('.overlay').remove();
+// }
 
 let cardContainer = document.querySelector('.card-container');
 let cards = document.querySelectorAll('.card');
 
 let TRANSITION_DURATION = 500
 let TIMEOUT_DURATION = TRANSITION_DURATION * 2;
+
+//filling cards with data-imgid 
+//(filling each two consecutive cards with the same image id)
+cards.forEach((card, i) => {
+    if (card.getAttribute('data-imgid') == null) {
+        let imgId = Math.ceil(Math.random() * 100) * 10;
+        card.setAttribute('data-imgid', imgId);
+        cards[i + 1].setAttribute('data-imgid', imgId);
+    }
+})
+//settign cards background
+let backImgSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--card-size'));
+document.querySelectorAll('.card .back').forEach(cardBack => {
+    let id = cardBack.parentElement.dataset.imgid;
+    cardBack.style.backgroundImage = `url('https://picsum.photos/id/${id}/${backImgSize}')`;
+})
 
 shuffle();
 
@@ -53,8 +64,8 @@ function handleMatch(card1, card2) {
 
 function shuffle() {
     let orderArr = [];
-    while (orderArr.length !== 20) {
-        let random = Math.floor(Math.random() * 20);
+    while (orderArr.length !== cards.length) {
+        let random = Math.floor(Math.random() * cards.length);
         if (!(orderArr.includes(random))) {
             orderArr.push(random);
         }
@@ -62,4 +73,27 @@ function shuffle() {
     cards.forEach((card, i) => {
         card.style.order = orderArr[i]
     })
+}
+
+function setDimensions(difficulty) {
+    switch (difficulty) {
+        case 'easy':
+            document.documentElement.style.setProperty('--container-width', '600px');
+            document.documentElement.style.setProperty('--container-height', '480px');
+            document.documentElement.style.setProperty('--card-size', '100px');
+            document.documentElement.style.setProperty('--card-margin', '10px');
+            break;
+        case 'intermediate':
+            document.documentElement.style.setProperty('--container-width', '546px');
+            document.documentElement.style.setProperty('--container-height', '455px');
+            document.documentElement.style.setProperty('--card-size', '75px');
+            document.documentElement.style.setProperty('--card-margin', '8px');
+            break;
+        case 'hard':
+            document.documentElement.style.setProperty('--container-width', '480px');
+            document.documentElement.style.setProperty('--container-height', '360px');
+            document.documentElement.style.setProperty('--card-size', '50px');
+            document.documentElement.style.setProperty('--card-margin', '5px');
+            break;
+    }
 }
