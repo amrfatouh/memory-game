569e545188f3133632068551dd4e5e22ff4e27e2
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-09-03 13:55:22 +0200

    implemented difficulties
    divided the project into functions to make building new levels for differenct difficulties easier

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-//starting thr game
-// document.querySelector('.overlay button').onclick = () => {
-//     document.querySelector('.info .name span').textContent = prompt('Enter your name: ');
-//     document.querySelector('.overlay').remove();
-// }
+//starting the game
+document.querySelector('.overlay button').onclick = () => {
+    document.querySelector('.info .name span').textContent = prompt('Enter your name: ') || 'unnamed';
+    GAME_DIFFICULTY = document.querySelector('.overlay select').value;
+    setUpGame(GAME_DIFFICULTY);
+    document.querySelector('.overlay').remove();
+}
 
 
 
 let cardContainer = document.querySelector('.card-container');
-let cards = document.querySelectorAll('.card');
+let cards = document.querySelectorAll('.card.playable');
+let imageIdArr = makeImageIdArr();
 
 let TRANSITION_DURATION = 500
 let TIMEOUT_DURATION = TRANSITION_DURATION * 2;
-let GAME_DIFFICULTY = 'easy'
+let GAME_DIFFICULTY = document.querySelector('.overlay select').value;
+
+setUpGame(GAME_DIFFICULTY);
 
 //choosing difficulty
 document.querySelectorAll('.options input').forEach(radio => {
@@ -19,51 +24,62 @@ document.querySelectorAll('.options input').forEach(radio => {
         if (confirm('Your progress will be erased. Are you sure?')) {
             setUpGame(radio.dataset.difficulty);
         } else {
-            // radio.checked = false;
             document.querySelector(`.options input[data-difficulty=${GAME_DIFFICULTY}]`).checked = true;
         }
     }
 })
 
-setUpGame('easy');
+
+// document.body.onblur = function () {
+//     alert('gone away ... reloading');
+//     window.location.reload();
+// }
+
 
 //making an array of valid IDs in the website picsum.photos
-let imageIdArr = Array.from(Array(80).keys()).map(x => x + 1000);
-imageIdArr.forEach((x, i) => {
-    if ([1007, 1017, 1030, 1034, 1046].includes(x)) {
-        imageIdArr.splice(i, 1);
-    }
-})
-function chooseId() {
-    let random = Math.floor(Math.random() * imageIdArr.length)
-    return imageIdArr.splice(random, 1)[0];
+function makeImageIdArr() {
+    let imageIdArr = Array.from(Array(80).keys()).map(x => x + 1000);
+    let forbiddenArr = [1007, 1017, 1030, 1034, 1046];
+    imageIdArr.forEach((x, i) => {
+        if (forbiddenArr.includes(x)) {
+            imageIdArr.splice(i, 1);
+        }
+    })
+    return imageIdArr;
+}
+function chooseIdFromArr(arr) {
+    let random = Math.floor(Math.random() * arr.length)
+    return arr.splice(random, 1)[0];
 }
 //filling cards with data-imgid 
 //(filling each two consecutive cards with the same image id)
-cards.forEach((card, i) => {
-    if (card.getAttribute('data-imgid') == null) {
-        let imgId = chooseId();
-        card.setAttribute('data-imgid', imgId);
-        cards[i + 1].setAttribute('data-imgid', imgId);
-    }
-})
-
+function fillCardsWithId() {
+    cards.forEach((card, i) => {
+        if (card.getAttribute('data-imgid') == null) {
+            let imgId = chooseIdFromArr(imageIdArr);
+            card.setAttribute('data-imgid', imgId);
+            cards[i + 1].setAttribute('data-imgid', imgId);
+        }
+    })
+}
 
 //settign cards background
-let backImgSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--card-size'));
-document.querySelectorAll('.card .back').forEach(cardBack => {
-    let id = cardBack.parentElement.dataset.imgid;
-    cardBack.style.backgroundImage = `url('https://picsum.photos/id/${id}/${backImgSize}')`;
-})
-
-shuffle();
-
-document.querySelectorAll('.card-container .card').forEach(card => {
-    card.addEventListener('click', () => {
-        card.classList.add('flipped');
-        checkIf2();
+function setCardsBackgroundFromData() {
+    let backImgSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--card-size'));
+    document.querySelectorAll('.card .back').forEach(cardBack => {
+        let id = cardBack.parentElement.dataset.imgid;
+        cardBack.style.backgroundImage = `url('https://picsum.photos/id/${id}/${backImgSize}')`;
     })
-})
+}
+
+function addEventListenersForCards() {
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.add('flipped');
+            checkIf2();
+        })
+    })
+}
 
 function checkIf2() {
     let flippedCards = document.querySelectorAll('.card.flipped');
@@ -130,16 +146,15 @@ function setDimensions(difficulty) {
 
 //creating all cards
 function createCards(num) {
-    cards.forEach((card, i) => {
-        if (i != 0) card.remove();
-    })
+    cards.forEach((card, i) => card.remove())
     let cardModel = document.querySelector('.card');
     for (let i = 0; i < num; i++) {
         let newCard = cardModel.cloneNode(true);
+        newCard.classList.add('playable');
         newCard.style.display = 'block';
         cardContainer.appendChild(newCard);
     }
-    cardModel.remove();
+    // cardModel.remove();
 }
 
 function setUpGame(difficulty) {
@@ -156,5 +171,10 @@ function setUpGame(difficulty) {
             createCards(48);
             break;
     }
-    cards = document.querySelectorAll('.card');
+    cards = document.querySelectorAll('.card.playable');
+    addEventListenersForCards();
+    fillCardsWithId();
+    setCardsBackgroundFromData();
+    shuffle();
+    document.querySelector('.info span.tries-num').textContent = 0;
 }
